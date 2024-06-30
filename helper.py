ec772dcf14ee2b149ae77ccf00e122b1eb702022
Ritwika2003 <144425242+Ritwika2003@users.noreply.github.com>
2024-06-30 12:17:42 +0530

    helper.py

--- helper.py
+++ helper.py
@@ -18,26 +18,7 @@ def fetch_stats(selected_user,df):
     links=[]
     for message in df['message']:
         links.extend(extract.find_urls(message))
-    return num_messages, len(words), num_media_messages, len(links)
-
-
-
-    if selected_user =='Overall':
-        #fetch number of messages
-        num_messages = df.shape[0]
-        #fetch number of words
-        words=[]
-        for message in df['message']:
-            words.extend(message.split())
-        return num_messages, len(words)
-    else:
-        new_df= df[df['user']== selected_user]
-        num_messages=new_df.shape[0]
-        words=[]
-        for message in df['message']:
-            words.extend(message.split())
-        return num_messages, len(words)
-
+    return num_messages, len(words), num_media_messages, len(links)    
 
 def most_busy_users(df):
     x=df['user'].value_counts().head()
